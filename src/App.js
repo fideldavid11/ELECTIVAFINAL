b1d0a775986d7d0d7909ce7abeb61b8a9da519ee
Fidel David Peguero <20210620@itla.edu.do>
2024-04-12 21:21:59 -0400

    Update testing dependen

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="bg-blue-500 text-white min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl">¡Hola Mundo!</h1>
+    <div className="bg-blue-500">
+      <p className="text-white text-4xl">¡Hola Mundo!</p>
     </div>
   );
 }
+
 
 export default App;
